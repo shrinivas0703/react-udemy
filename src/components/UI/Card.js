ea1069ae2c48506f277b20b7d152c2d6e6ea5a39
Shrinivas Venkatesan <89709558+shrinivas0703@users.noreply.github.com>
2022-07-06 18:20:27 -0400

    Update Card.js

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ function Card(props) {
     const classes = "card " + props.className;
     return (
         <div className = {classes}>{props.children}</div>
-    )
+    );
 }
 
 export default Card;
